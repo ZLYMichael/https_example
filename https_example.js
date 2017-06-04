c3b119d590fa878cdf0f796142a11ca2215111f9
vagrant <zlymike@outlook.com>
2017-06-04 04:35:56 +0000

    example.com

--- https_example.js
+++ https_example.js
@@ -1,3 +1,15 @@
 var https = require('https');
 
-console.log('I did it!');
+var options = {
+  host:'www.example.org',
+  path: '/'
+};
+
+var callback = function() {
+  console.log('In response handler callback!');
+}
+
+console.log ("i'm about to make the request!");
+https.request(options, callback).end();
+
+console.log('I made the request!');
